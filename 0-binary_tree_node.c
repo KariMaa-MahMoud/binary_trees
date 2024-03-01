29679d7815cc98b5ff8248ad4c97c5a000f0caf5
KariMaa-MahMoud <karima.mahmoud10@gmail.com>
2024-03-01 20:09:02 +0200

    all mandatory tasks2

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,18 +7,14 @@
  * 
  * Return: Pointer to new node, NULL on failure
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
 	return (new_node);
 }
